1c0a1efb35b8faac585868d145cfd22f672c0e31
David Cabo <david.cabo@gmail.com>
2015-05-30 19:17:21 +0200

    Add note about census figures

--- parse.rb
+++ parse.rb
@@ -1,6 +1,18 @@
 require 'csv'
 require 'nokogiri'
 
+# Nota sobre las cifras del censo electoral:
+# Ejecutamos los scripts por primera vez durante la madrugada electoral, con el escrutinio al ~99.5%.
+# Unos días después, ya al 100%, repetimos todo el proceso, y vemos que el censo ha subido en algunos
+# municipios (<20), que no habían terminado el recuento en la primera pasada.
+# El motivo es que calculamos el censo electoral como el número de votos más la abstención. Ésta última
+# sí es final, porque viene -imagino- de las cifras de participación, que entiendo son finales desde que
+# se cierran las mesas (aunque habría que comprobarlo en las próximas elecciones). En cambio el número de
+# votos va subiendo según avanza el recuento, obviamente.
+# Todo esto tiene poca relevancia práctica: estamos obligados a esperar a que termine el recuento para
+# poder calcular las subvenciones, sí o sí, porque necesitamos el número de votos. Pero es importante
+# aclarar que el censo tampoco es final, lo cual es contraintuitivo a primera vista.
+
 town_data = CSV.open('town_data.csv', "w")
 town_data << [
   'ID',
